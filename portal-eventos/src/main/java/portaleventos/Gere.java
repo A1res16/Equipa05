f86ff77201bc8c68307a9cdeff8496d24816aab4
NobreEng <pedro.nobre.eng@gmail.com>
2025-10-28 15:44:22 +0000

    por verificar... 2ºparte

--- portal-eventos/src/main/java/portaleventos/Gere.java
+++ portal-eventos/src/main/java/portaleventos/Gere.java
@@ -1,6 +1,48 @@
 package portaleventos;
 
-public class Gere 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+
+public class Gere
 {
-   
+    SessionFactory sessionFactory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .buildSessionFactory();
+    Session session = sessionFactory.openSession();
+
+    public void exit()
+    {
+        if (sessionFactory != null)
+        {
+            sessionFactory.close();
+        }
+    }
+
+    public void create(String tituloEvento, String nomePromotores, LocalDate data, int duracao, String local, String descricaoBreve, Enum TipoEvento, Enum Departamento, int orcamento, int capacidade, Enum Estado)
+    {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        // --- Create Pedidos ---
+        Pedido p1 = new Pedido(tituloEvento, nomePromotores, data, duracao, local, descricaoBreve, TipoEvento, Departamento, orcamento, capacidade, Estado);
+        session.persist(p1);
+
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("Pedido criado!");
+    }
+
+
+
+
+
+    public void remove(Pedido p1)
+    {
+        session.remove(p1);
+        System.out.println("Pedido removido...");
+    }
+
+
 }
--- portal-eventos/src/main/java/portaleventos/Pedido.java
+++ portal-eventos/src/main/java/portaleventos/Pedido.java
@@ -13,19 +13,44 @@ public class Pedido
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String tituloEvento;
+
+    @Column(nullable = false)
     private String nomePromotores;
+
+    @Column(nullable = false)
     private LocalDate data;
+
     //Duração em MINUTOS  (IMPLEMENTAR NAS USER STORIES)
+    @Column(nullable = false)
     private int duracao;
+
+    @Column(nullable = false)
     private String local;
+
+    @Column(nullable = false)
     private String descricaoBreve;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Enum TipoEvento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Enum Departamento;
+
     //Orçamento em euros;
+    @Column(nullable = false)
     private int orcamento;
+
+    @Column(nullable = false)
     private int capacidade;
+
+    @Column(nullable = false)
     private Enum Estado;
+
     @ManyToOne
     @JoinTable(
             name = "promotor_pedido",
@@ -35,5 +60,17 @@ public class Pedido
     private Set<Promotor> promotores = new HashSet<>();
 
     public Pedido() {
+    }
+
+    public Pedido(String tituloEvento, String nomePromotores, LocalDate data, int duracao, String local, String descricaoBreve, Enum TipoEvento, Enum Departamento, int orcamento, int capacidade, Enum Estado) {
+        this.tituloEvento = tituloEvento;
+        this.nomePromotores = nomePromotores;
+        this.data = data;
+        this.duracao = duracao;
+        this.local = local;
+        this.descricaoBreve = descricaoBreve;
+        this.TipoEvento = TipoEvento;
+        this.Departamento = Departamento;
+        this.orcamento = orcamento;
     }
 }
